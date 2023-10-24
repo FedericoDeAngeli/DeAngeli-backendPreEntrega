2bfe7b3fb9b7b3ae18b25b2652431b5eaace5494
Federico Jose De Angeli <fdeangeli_90@hotmail.com>
2023-10-24 19:40:24 -0300

    Agregados update fs

--- entrega1.js
+++ entrega1.js
@@ -1,3 +1,7 @@
+const {promises:fs} = require("fs")
+const { title } = require("process")
+
+
 class Product {
 id
 title
@@ -21,17 +25,61 @@ constructor({title, description, price, thumbnail, code, stock}){
 class productManager {
 static productsId = 0
 
-products = []
+constructor({ruta}){
+this.path = ruta
+this.products = []
+}
+async init(){
+    await this.writeProduct()
+}
 
 static generarId(){
     return ++productManager.productsId
 }
 
-getProducts(){
+async readProduct(){
+    const productJSON = await fs.readFile(this.path, "utf-8")
+    this.products = JSON.parse(productJSON)
+}
+
+async writeProduct(){
+    
+    await fs.writeFile(this.path, JSON.stringify(this.products)) 
+}catch(error){
+    console.log(error.message)
+}
+
+
+async updateProduct(id, {title, description, price, thumbnail, code, stock}){
+    await this.readProduct()
+    const index = this.products.findIndex((e)=> e.id === id)
+    if(index !== -1){
+        this.products[index] = new Product({
+        id, title, description, price, thumbnail, code, stock })
+        
+        await this.writeProduct()
+        return this.products
+         
+    }else{
+        console.log("Producto no encontrado")
+    }
+}
+
+async deleteProduct(){
+    const index = this.products.findIndex((e)=> e.id === id)
+    if(index !== -1){
+        await fs.unlink(this.path, index)
+        return this.products
+    }
+}
+
+
+async getProducts(){
+    await this.readProduct()
     return this.products
 }
 
-addProduct({title, description, price, code, thumbnail, stock}){
+async addProduct({title, description, price, code, thumbnail, stock}){
     if(!title || !description || !price || !code || !thumbnail || !stock){
         console.log("Es obligatorio llenar todos los campos")
     }else{
@@ -41,15 +89,17 @@ addProduct({title, description, price, code, thumbnail, stock}){
     
     const id = productManager.generarId();
     const product = new Product({id, title, description, price,code, thumbnail, code, stock})
+    await this.readProduct()
     this.products.push(product)
+    await this.writeProduct()
     return product
 }
 }
 }
 
 
-getProductsById(id){
-    const productId = this.products.find((e) => e.id === id)
+ getProductsById(id){
+    const productId =  this.products.find((e) => e.id === id)
     if(!productId){
         console.log("Error al traer el producto")
     }else{
@@ -58,23 +108,33 @@ getProductsById(id){
 }
 }
 
-const pm = new productManager()
+async function main(){
+const pm = new productManager({ruta: "productManager.json"})
+pm.init()
 
 console.log(pm.getProducts())
 
-const p1 = pm.addProduct ({title: "Curcuma",description: "Adobo", price: "150", code:"1", thumbnail: "img1", stock: "20"})
-const p2 = pm.addProduct ({description: "Molida", price: "250", code:"2",thumbnail: "img2", stock: "10"})   
-const p3 = pm.addProduct ({title: "Azafrán",description: "Egipcio", price: "1200", code:"1",thumbnail: "img3", stock: "5"})
-const p4 = pm.addProduct ({title: "Pimentón",description: "Español", price: "300", code:"4",thumbnail: "img4", stock: "22"})
+const p1 = await pm.addProduct ({title: "Curcuma",description: "Adobo", price: "150", code:"1", thumbnail: "img1", stock: "20"})
+const p2 = await pm.addProduct ({description: "Molida", price: "250", code:"2",thumbnail: "img2", stock: "10"})   
+const p3 = await pm.addProduct ({title: "Azafrán",description: "Egipcio", price: "1200", code:"1",thumbnail: "img3", stock: "5"})
+const p4 = await pm.addProduct ({title: "Pimentón",description: "Español", price: "300", code:"4",thumbnail: "img4", stock: "22"})
+const p5 = await pm.addProduct ({title: "Cereal",description: "Ositos", price: "700", code:"6",thumbnail: "img6", stock: "18"})
+const p6 = await pm.addProduct ({title: "Orégano",description: "Especial", price: "410", code:"7",thumbnail: "img7", stock: "32"})
 
 
-console.log(pm.getProducts())
+// console.log(pm.getProducts())
 
-const productFound = pm.getProductsById(1)
+// const productFound = pm.getProductsById(1)
 
-console.log(productFound)
+// console.log(productFound)
 
-const productNotFound = pm.getProductsById(5)
+// const productNotFound = pm.getProductsById(12)
 
-console.log(productNotFound)
+// console.log(productNotFound)
 
+pm.updateProduct(3, {title:"Aji cayena",description: "picante", price: "450", thumbnail:"img5",code: "5", stock: "26"})
+pm.deleteProduct(1)
+
+ }
+
+main()
